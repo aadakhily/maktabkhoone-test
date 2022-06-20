25aa866c0bdf8e78e9854050e52b0dd0a055e98f
Amir Abbas Dakhily <a.a.dakhily@gmail.com>
2022-06-20 22:12:22 +0430

    feat/init project and complete ui

---
+++ src/App/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app">
+    <header class="app__header">
+      <h1>Profiles List</h1>
+    </header>
+
+    <main class="app__content">
+      <ProfileCart
+        v-for="profile in profiles"
+        :profile="profile"
+        :key="profile.id"
+      />
+    </main>
+  </div>
+</template>
+
+<script>
+import ProfileCart from "@/components/ProfileCard/index.vue";
+
+import data from "../../data.json";
+export default {
+  name: "TheApp",
+
+  components: {
+    ProfileCart,
+  },
+
+  data() {
+    return {
+      profiles: [],
+    };
+  },
+
+  methods: {
+    fetchData() {
+      this.profiles = data.profiles;
+    },
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
+
+<style src="./index.scss" lang="scss" />
---
+++ src/components/ProfileCard/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="profile-card">
+    <img
+      src="@/assets/images/doctor.svg"
+      class="profile-card__image"
+      :alt="`Doctor-${profile.name}`"
+    />
+
+    <div class="profile-card__info">
+      <h4 class="profile-card__name">
+        <span class="label">Name: </span>
+        {{ profile.name }}
+      </h4>
+      <span class="profile-card__email">
+        <span class="label">Email: </span>
+        {{ profile.email }}
+      </span>
+      <p class="profile-cart__description">
+        <span class="label"> Description: </span> {{ profile.description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileCard",
+
+  props: {
+    profile: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isFollowing: false,
+    };
+  },
+};
+</script>
+
+<style src="./index.scss" lang="scss" scoped />
