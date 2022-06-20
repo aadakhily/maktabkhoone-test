16e2fb99d768c44409b74caef0084240b136d077
Amir Abbas Dakhily <a.a.dakhily@gmail.com>
2022-06-21 01:02:28 +0430

    feat/create serach input ui

--- src/App/index.vue
+++ src/App/index.vue
@@ -2,6 +2,21 @@
   <div class="app">
     <header class="app__header">
       <h1>Profiles List</h1>
+      <div class="search-container">
+        <div class="search">
+          <div class="search__input">
+            <SearchIcon />
+
+            <input type="text" placeholder="Search..." />
+          </div>
+
+          <div class="search__sort">
+            <TopArrow class="icon" />
+            <BottomArrow class="icon" />
+          </div>
+        </div>
+        <button class="new-profile-btn">New Profile</button>
+      </div>
     </header>
 
     <main class="app__content">
@@ -15,13 +30,24 @@
 </template>
 
 <script>
+//fake data
+import data from "../../data.json";
+
+// components
 import ProfileCart from "@/components/ProfileCard/index.vue";
 
-import data from "../../data.json";
+// icon components
+import TopArrow from "@/components/icons/TopArrow.vue";
+import SearchIcon from "@/components/icons/SearchIcon.vue";
+import BottomArrow from "@/components/icons/BottomArrow.vue";
+
 export default {
   name: "TheApp",
 
   components: {
+    TopArrow,
+    SearchIcon,
+    BottomArrow,
     ProfileCart,
   },
 
