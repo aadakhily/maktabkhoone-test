14690caf615fd565b29ffe572417661d28f1fdeb
Amir Abbas Dakhily <a.a.dakhily@gmail.com>
2022-06-20 23:42:13 +0430

    feat/profile card comment section

--- src/components/ProfileCard/index.vue
+++ src/components/ProfileCard/index.vue
@@ -1,30 +1,59 @@
 <template>
   <div class="profile-card">
-    <img
-      src="@/assets/images/doctor.svg"
-      class="profile-card__image"
-      :alt="`Doctor-${profile.name}`"
-    />
+    <div class="profile-card-details">
+      <div>
+        <img
+          src="@/assets/images/doctor.svg"
+          class="profile-card__image"
+          :alt="`Doctor-${profile.name}`"
+        />
+      </div>
 
-    <div class="profile-card__info">
-      <h4 class="profile-card__name">
-        <span class="label">Name: </span>
-        {{ profile.name }}
-      </h4>
-      <span class="profile-card__email">
-        <span class="label">Email: </span>
-        {{ profile.email }}
-      </span>
-      <p class="profile-cart__description">
-        <span class="label"> Description: </span> {{ profile.description }}
-      </p>
+      <div class="profile-card-info">
+        <h4 class="profile-card__name">
+          <span class="label">Name: </span>
+          {{ profile.name }}
+        </h4>
+        <span class="profile-card__email">
+          <span class="label">Email: </span>
+          {{ profile.email }}
+        </span>
+        <p class="profile-cart__description">
+          <span class="label"> Description: </span> {{ profile.description }}
+        </p>
+        <div class="profile-card__reaction">
+          <component
+            :is="heartIcon"
+            class="profile-card__heart-icon"
+            @click="like"
+          />
+
+          <span class="profile-card__likes-count">{{ profile.likes }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="profile-card__comment">
+      <input
+        type="text"
+        class="profile-card__comment-input"
+        placeholder="Write your comment..."
+      />
+      <button class="profile-card__comment-button">Send</button>
     </div>
   </div>
 </template>
 
 <script>
+import HeartFilled from "@/components/icons/HeartFilled.vue";
+import HeartOutline from "@/components/icons/HeartOutline.vue";
+
 export default {
   name: "ProfileCard",
+
+  components: {
+    HeartFilled,
+    HeartOutline,
+  },
 
   props: {
     profile: {
@@ -33,10 +62,22 @@ export default {
     },
   },
 
+  computed: {
+    heartIcon() {
+      return this.isLiked > 0 ? "HeartFilled" : "HeartOutline";
+    },
+  },
+
   data() {
     return {
-      isFollowing: false,
+      isLiked: false,
     };
+  },
+
+  methods: {
+    like() {
+      this.isLiked = !this.isLiked;
+    },
   },
 };
 </script>
