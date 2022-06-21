7d89188ad4b80dd13a2e9f2313f3b19d4faf131f
Amir Abbas Dakhily <a.a.dakhily@gmail.com>
2022-06-21 13:20:28 +0430

    feat/search input and list transition

--- src/App/index.vue
+++ src/App/index.vue
@@ -2,29 +2,33 @@
   <div class="app">
     <header class="app__header">
       <h1>Profiles List</h1>
+
       <div class="search-container">
         <div class="search">
           <div class="search__input">
             <SearchIcon />
 
-            <input type="text" placeholder="Search..." />
+            <input type="text" placeholder="Search..." v-model="searchInput" />
           </div>
 
           <div class="search__sort">
-            <TopArrow class="icon" />
-            <BottomArrow class="icon" />
+            <TopArrow class="icon" @click="sortAsc" />
+            <BottomArrow class="icon" @click="sortDesc" />
           </div>
         </div>
+
         <button class="new-profile-btn">New Profile</button>
       </div>
     </header>
 
     <main class="app__content">
-      <ProfileCart
-        v-for="profile in profiles"
-        :profile="profile"
-        :key="profile.id"
-      />
+      <TransitionGroup name="list">
+        <ProfileCart
+          v-for="profile in searchResult"
+          :profile="profile"
+          :key="profile.id"
+        />
+      </TransitionGroup>
     </main>
   </div>
 </template>
@@ -54,6 +58,7 @@ export default {
   data() {
     return {
       profiles: [],
+      searchInput: "",
     };
   },
 
@@ -61,9 +66,25 @@ export default {
     fetchData() {
       this.profiles = data.profiles;
     },
+
+    sortAsc() {
+      this.profiles.sort((a, b) => a.likes - b.likes);
+    },
+
+    sortDesc() {
+      this.profiles.sort((a, b) => b.likes - a.likes);
+    },
   },
 
-  mounted() {
+  computed: {
+    searchResult() {
+      return this.profiles.filter((item) =>
+        item.name.includes(this.searchInput)
+      );
+    },
+  },
+
+  beforeMount() {
     this.fetchData();
   },
 };
